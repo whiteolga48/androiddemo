74241e486db4664b55e1182eefcf8cbd6e650aea
olga millan <olga.millan@gmail.com>
2019-06-16 23:57:39 +0200

    Añadido módulo Tablayout

---
+++ tablayout/src/main/java/com/mariusapps/myapplication/FragmentOlga.java
@@ -0,0 +1,4 @@
+package com.mariusapps.myapplication;
+
+public class FragmentOlga {
+}
---
+++ tablayout/src/main/java/com/mariusapps/myapplication/FragmentQuiz.java
@@ -0,0 +1,29 @@
+package com.mariusapps.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentQuiz extends Fragment {
+
+    View view;
+
+
+    public FragmentQuiz() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        view = inflater.inflate( R.layout.quiz_fragment, container, false);
+
+        return view;
+    }
+}
